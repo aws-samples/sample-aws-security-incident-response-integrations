091e6f6b72c2c24185cb84d92accae5805ce41c2
APKAV5SXJILFECPUAOS6 <dnfranke@amazon.com>
2025-07-22 10:12:02 -0500

    Added support for Service Now

--- assets/security_ir_client/index.py
+++ assets/security_ir_client/index.py
@@ -18,7 +18,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 UPDATE_TAG_TO_SKIP = "[AWS Security Incident Response Update]"
-UPDATE_TAG_TO_ADD = "[JIRA Update]"
+ITSM_NAME = "TBD"
 
 # Try to import from Lambda layer
 try:
@@ -34,9 +34,22 @@ except ImportError:
     from ..mappers.python.jira_sir_mapper import (
         Case,
         create_case_from_api_response,
-        map_fields_to_sir,
+        map_fields_to_sir as map_jira_fields_to_sir,
     )
     from ..wrappers.python.security_ir_wrapper import SecurityIRClient
+
+try:
+    from service_now_sir_mapper import (
+        map_case_status,
+        map_security_now_fields_to_sir,
+        map_closure_code,
+    )
+except ImportError:
+    from ..mappers.python.service_now_sir_mapper import (
+        Case,
+        create_case_from_api_response,
+        map_security_now_fields_to_sir,
+    )
 
 # Get log level from environment variable
 log_level = os.environ.get("LOG_LEVEL", "error").lower()
@@ -55,6 +68,66 @@ logger.setLevel(logging.INFO)
 security_ir_client = boto3.client("security-ir")
 
 
+def process_service_now_event(security_now_incident: dict) -> None:
+    incident_service = IncidentService()
+    database_service = DatabaseService()
+
+    logger.info("Processing ServiceNow event")
+    security_ir_fields = map_security_now_fields_to_sir(security_now_incident)
+    service_now_incident_id = security_now_incident["number"]
+
+    security_ir_status = map_case_status(security_now_incident["state"])
+    # join dict objects
+    security_ir_fields = security_ir_fields | {"status": security_ir_status[0]}
+    # add fields to incident object
+    security_ir_fields["key"] = service_now_incident_id
+    security_ir_fields["source"] = "servicenow"
+
+    # determine if this is an incident created or incident updated event
+    event_type = security_now_incident["event_type"]
+
+    if "created" in event_type.lower():
+        security_ir_case_id = incident_service.create_incident_in_sir(
+            security_ir_incident=security_ir_fields
+        )
+        security_ir_fields["caseId"] = security_ir_case_id
+        logger.info("New Security IR case created: %s", security_ir_case_id)
+
+        # get latest security_ir now that all fields have been updated
+        #  and store it in the database
+        security_ir_incident = incident_service.get_incident_from_sir(
+            security_ir_case_id
+        )
+
+        if security_ir_incident:
+            security_ir_incident["caseId"] = security_ir_case_id
+            database_service.store_issue_details_in_dynamodb(security_ir_incident, "servicenow")
+
+    elif "updated" in event_type.lower():
+        # if it's an update then an entry for the incident must already exist in the database
+        security_ir_case_id = database_service.get_incident_id_from_dynamodb(
+            issue_id=service_now_incident_id, event_source="servicenow"
+        )
+
+        if security_ir_case_id:
+            security_ir_fields["caseId"] = security_ir_case_id
+            _ = incident_service.update_incident_details_in_sir(
+                security_ir_case=security_ir_fields
+            )
+
+        # get latest security_ir now that all fields have been updated
+        #  and store it in the database
+        security_ir_incident = incident_service.get_incident_from_sir(
+            security_ir_case_id
+        )
+
+        if security_ir_incident:
+            security_ir_incident["caseId"] = security_ir_case_id
+            database_service.store_issue_details_in_dynamodb(security_ir_incident, "servicenow")
+
+    return
+
+
 def process_jira_event(jira_issue: dict) -> None:
     """
     Creates or updates Security Incident Response Case based on an incoming Jira Issue details
@@ -65,6 +138,8 @@ def process_jira_event(jira_issue: dict) -> None:
     Returns:
         None
     """
+    logger.info("Processing Jira event")
+
     # map Jira issue to Security Incident Response case
     jira_event_type = jira_issue["eventType"]
     jira_issue_key = jira_issue["key"]
@@ -107,8 +182,7 @@ def process_jira_event(jira_issue: dict) -> None:
     elif jira_event_type == "IssueUpdated":
         # get case ID from ddb
         security_ir_case_id = database_service.get_incident_id_from_dynamodb(
-            jira_issue_id=jira_issue_key
-        )
+            issue_id=jira_issue_key, event_source="jira")
 
         if security_ir_case_id:
             security_ir_fields["caseId"] = security_ir_case_id
@@ -197,36 +271,43 @@ def process_jira_event(jira_issue: dict) -> None:
     )
     if security_ir_incident:
         security_ir_incident["caseId"] = security_ir_case_id
-        database_service.store_incident_in_dynamodb(security_ir_incident)
+        database_service.store_issue_details_in_dynamodb(security_ir_incident, "jira")
+
 
 class DatabaseService:
     """Class to handle database operations"""
-
     __dynamodb = boto3.resource("dynamodb")
     __table_name = os.environ["INCIDENTS_TABLE_NAME"]
     __ddb_table = __dynamodb.Table(__table_name)
     __dynamodb_client = boto3.client("dynamodb")
+    #TODO: use dynamodb resource only
 
     def __init__(self):
         """Initialize the database manager"""
 
-    def get_incident_id_from_dynamodb(self, jira_issue_id: str) -> str:
+
+    def get_incident_id_from_dynamodb(self, issue_id: str, event_source: str) -> str:
         """
         Fetch Case Id associated with Issue Id in Jira
 
         Args:
-            Jira Issue Id
+            Issue Id
 
-        Returns:
+        Returns:create_incident_in_sir
             Security Incident Response Case Id
         """
+        attr_name = ""
+        if event_source == "jira":
+            attr_name = "jiraIssueId"
+        elif event_source == "servicenow":
+            attr_name = "serviceNowIncidentId"
         try:
             response = self.__ddb_table.scan(
-                FilterExpression=Attr("jiraIssueId").eq(jira_issue_id), Limit=1000
+                FilterExpression=Attr(attr_name).eq(issue_id), Limit=1000
             )
             if response["Items"] == []:
                 logger.info(
-                    f"Security IR case for Jira issue {jira_issue_id} not found in database"
+                    f"Security IR case for {event_source} issue {issue_id} not found in database"
                 )
                 security_ir_case_id = None
             else:
@@ -249,13 +330,13 @@ class DatabaseService:
             return None
 
 
-    def store_incident_in_dynamodb(self, incident: dict) -> bool:
+    def store_issue_details_in_dynamodb(self, incident: dict, event_source: str) -> bool:
         """
         Store or update incidents in DynamoDB
 
         Args:
             incidents: List of incidents to store
-            table_name: Name of the DynamoDB table
+            event_source: source of event (ITSM)
 
         Returns:
             Boolean indicating success or failure
@@ -283,6 +364,11 @@ class DatabaseService:
                     existing_details = json.loads(
                         existing_incident.get("incidentDetails", {}).get("S", "{}")
                     )
+                    if event_source == "jira":
+                        field_name = "jiraIssueDetails"
+                    if event_source == "servicenow":
+                        field_name = "serviceNowIncidentDetails"
+                    
                     if existing_details != incident:
                         self.__dynamodb_client.update_item(
                             TableName=self.__table_name,
@@ -339,10 +425,11 @@ class IncidentService:
     """Class to handle security IR incident operations"""
 
     __database_service = DatabaseService()
+    __security_ir_client = boto3.client("security-ir")
+    #TODO: use SecurityIRClient wrapper instead
 
     def __init__(self):
         """Initialize the incident manager"""
-        self.ir_client = SecurityIRClient()
 
     def update_incident_details_in_sir(self, security_ir_case: dict) -> bool:
         """
@@ -375,14 +462,21 @@ class IncidentService:
                 "title": security_ir_case["title"],
                 "description": security_ir_case["description"],
             }
-            _ = security_ir_client.update_case(**request_kwargs)
+            _ = self.__security_ir_client.update_case(**request_kwargs)
 
+        except Exception as e:
+            logger.error(
+                f"Error updating Security IR case {security_ir_case_id} details: {str(e)}"
+            )
+            return False
+        
+        try:
             # update case status
             _ = self.update_incident_status_in_sir(security_ir_case)
 
         except Exception as e:
             logger.error(
-                f"Error updating Security IR case {security_ir_case_id}: {str(e)}"
+                f"Error updating Security IR case {security_ir_case_id} status: {str(e)}"
             )
             return False
 
@@ -404,7 +498,7 @@ class IncidentService:
         if security_ir_case_status == "Closed":
             try:
                 request_kwargs = {"caseId": security_ir_case_id}
-                _ = security_ir_client.close_case(**request_kwargs)
+                _ = self.__security_ir_client.close_case(**request_kwargs)
                 logger.info(f"Closed Security IR case {security_ir_case_id}")
             except Exception as e1:
                 logger.error(
@@ -420,7 +514,7 @@ class IncidentService:
                 }
                 # TODO: Support different case status transitions so that case can be set
                 # to any status via update from Jira
-                update_result = security_ir_client.update_case_status(**request_kwargs)
+                update_result = self.__security_ir_client.update_case_status(**request_kwargs)
                 logger.info(
                     f"Updated status of Security IR case {security_ir_case_id}: {update_result}"
                 )
@@ -450,7 +544,7 @@ class IncidentService:
         # TODO: add pagination support for comments
 
         request_kwargs = {"caseId": security_ir_case_id, "maxResults": 25}
-        sir_comments = security_ir_client.list_comments(**request_kwargs)
+        sir_comments = self.__security_ir_client.list_comments(**request_kwargs)
 
         return sir_comments
 
@@ -470,7 +564,7 @@ class IncidentService:
 
         try:
             request_kwargs = {"caseId": security_ir_case_id, "body": ir_case_comment}
-            _ = security_ir_client.create_case_comment(**request_kwargs)
+            _ = self.__security_ir_client.create_case_comment(**request_kwargs)
         except Exception as e:
             logger.error(
                 f"Error adding comment to Security IR case {security_ir_case_id}: {str(e)}"
@@ -484,7 +578,7 @@ class IncidentService:
         Create a new case in Security IR based on Jira issue
 
         Args:
-            Jira issue details
+            incident details
 
         Returns:
             Security IR case ID
@@ -492,6 +586,14 @@ class IncidentService:
         # create current datetime object
         current_datetime = datetime.datetime
         current_datetime_str = current_datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        event_source = security_ir_incident["source"].strip()
+        event_source_map = {
+            "servicenow": "ServiceNow",
+            "jira": "Jira",
+            "slack": "Slack"
+        }
+        
+        event_source_description = event_source_map.get(event_source, "Unknown")
 
         # 1. create case in Security IR
         try:
@@ -517,9 +619,10 @@ class IncidentService:
             security_ir_description = security_ir_incident.get(
                 "description", "Description not provided"
             )
-            security_ir_description += "\n\nThis Security Incident Response case was created as a result of a Jira issue being created."
+            
+            security_ir_description += f"\n\nThis Security Incident Response case was created as a result of a {event_source_description} issue being created."
             security_ir_description += (
-                f"\n\nRelated Jira issue: {security_ir_incident['key']}"
+                f"\n\nRelated {event_source_description} issue: {security_ir_incident['key']}"
             )
 
             request_kwargs = {
@@ -535,11 +638,11 @@ class IncidentService:
                 "impactedServices": ["TBD"],
             }
             logger.info(
-                "Required values not provided in Jira issue, using default values for Security IR case creation. Please update the Security IR case with actual values."
+                f"Required values not provided in {event_source_description} issue, using default values for Security IR case creation. Please update the Security IR case with actual values."
             )
 
             # get newly-created case
-            security_ir_case = security_ir_client.create_case(**request_kwargs)
+            security_ir_case = self.__security_ir_client.create_case(**request_kwargs)
             security_ir_case_id = security_ir_case["caseId"]
 
             # # add to database
@@ -547,7 +650,7 @@ class IncidentService:
             security_ir_incident["caseId"] = security_ir_case_id
 
         except Exception as e:
-            logger.error(f"Error creating Security IR case: {str(e)}")
+            logger.error(f"Error creating Security IR case: {e}")
             return None
 
         return security_ir_case_id
@@ -564,7 +667,7 @@ class IncidentService:
         """
         try:
             kwargs = {"caseId": security_ir_case_id}
-            security_ir_case = security_ir_client.get_case(**kwargs)
+            security_ir_case = self.__security_ir_client.get_case(**kwargs)
             return security_ir_case
 
         except Exception as e:
@@ -577,9 +680,9 @@ class IncidentService:
         self, security_ir_case_id: str, attachment_filename: str
     ) -> bool:
         """
-        Create a new case in Security IR based on Jira issue
+        Create a new case in Security IR based on ITSM issue
         For now we are going to add a comment as we need to get the attachment binary
-        from the Jira case in order to attach it
+        from the ITSM case in order to attach it
 
         Args:
             Security IR case ID
@@ -588,7 +691,7 @@ class IncidentService:
         Returns:
             True if add is successful, False otherwise
         """
-        comment = f"{UPDATE_TAG_TO_ADD} Jira issue has an attachment: {attachment_filename}. Download the file from the associated Jira issue."
+        comment = f"[{ITSM_NAME} Update] {ITSM_NAME} issue has an attachment: {attachment_filename}. Download the file from the associated ITSM issue."
 
         try:
             # TODO: add support to copy binary file attachment from Jira to Security IR
@@ -614,16 +717,29 @@ def handler(event, context) -> dict:
         Dictionary containing response status and details
     """
     # only process events from jira
-    EVENT_SOURCE = os.environ.get("EVENT_SOURCE", "jira")
+    # determine type of event to process it correctly
+    event_source = ""
+    try:
+        event_source = event["source"]
+    except:
+        logger.info("Event does not have source field")
 
-    if event["source"] == EVENT_SOURCE:
-        logger.info("Security Incident Response Client lambda will process this event")
-        process_jira_event(event)
+        try:
+            event_source = event["sys_id"]
+            event_source = "servicenow"
+        except:
+            logger.info("Event does not have sys_id field")
 
-    else:
-        logger.info(
-            "Security Incident Response Client lambda will skip processing of this event as the event source is not jira-notifications-handler"
-        )
+    if event_source == "jira":
+        ITSM_NAME = "Jira"
+        process_jira_event(event)        
+
+    elif event_source == "servicenow":
+        ITSM_NAME = "ServiceNow"
+        process_service_now_event(event)
+    
+    elif event_source == "slack":
+        ITSM_NAME = "Slack"
 
     return {
         "statusCode": 200,
